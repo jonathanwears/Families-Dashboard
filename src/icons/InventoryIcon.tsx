0189bf6524855583a4552764aa6c34efe76d6dd6
Jona <jonathanwears@outlook.com>
2023-02-17 16:44:06 +0100

    fixed prop names

--- src/icons/InventoryIcon.tsx
+++ src/icons/InventoryIcon.tsx
@@ -2,7 +2,7 @@
 function InventoryIcon() {
   return (
     <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M21.3333 13.3333H4M26.6667 8H4M26.6667 18.6667H4M21.3333 24H4" stroke="#6941C6" stroke-width="2.66667" stroke-linecap="round" stroke-linejoin="round"></path>
+      <path d="M21.3333 13.3333H4M26.6667 8H4M26.6667 18.6667H4M21.3333 24H4" stroke="#6941C6" strokeWidth="2.66667" strokeLinecap="round" strokeLinejoin="round"></path>
     </svg>
   )
 }
